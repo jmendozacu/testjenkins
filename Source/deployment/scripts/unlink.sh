7b5376ad09131c071e34bb197a54843ba2f8fc3d
yatin1493 <testaws123t@gmail.com>
2018-07-06 15:14:37 +0530

    Source folder removed

--- Source/deployment/scripts/unlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -a links=("/var/www/html/web/var", "/var/www/html/var/classes", "/var/www/html/var/config", "/var/www/html/var/sessions","/var/www/html/var/versions")
-
-for i in "${links[@]}"
-do
-   if [  -L $i ];then
-      unlink $i
-   fi
-done
